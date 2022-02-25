26f9218b5041629a87f47cad64584d577ddc5a47
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2022-02-25 13:51:35 +0500

    сохранила в локалсториж

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
+const TICK_TIME = 5;
 
 function App() {
-  
-    const [seconds, setSeconds] = useState(5);
-    const [isActive, setIsActive] = useState(false);
-  
-    function toggle() {
-      setIsActive(!isActive);
-    }
-  
-    function stop() {
-      setSeconds(5);
-      setIsActive(false);
-    }
-  
-    useEffect(() => {
-      let interval = null;
-      if (isActive) {
-        interval = setInterval(() => {
-          setSeconds(seconds => seconds - 1);
-        }, 1000);
-      } else if  (setSeconds === 0)  {
-        clearInterval(interval);
+  let savedTick = parseInt(localStorage.getItem("tick") || TICK_TIME)
+  const [seconds, setSeconds] = useState(savedTick);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function stop() {
+    setIsActive(false);
+    setSeconds(TICK_TIME);
+  }
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+
+      if (seconds === 0) {
+        stop()
       }
-      return () => clearInterval(interval);
-    }, [isActive, seconds]);
-  
+    } else {
+      clearInterval(interval);
+    }
 
-    
-    return (
-      <div className="app">
-        <div className="time">
-          {seconds}
-        </div>
-        <div className="row">
-          <button  className="toggle" onClick={toggle}>
-            {isActive ? 'Pause' : 'Start'}
-          </button>
-          <button className="stop" onClick={stop}>
-            Stop
-          </button>
-        </div>
+    localStorage.setItem("tick", seconds.toString())
+
+    return () => clearInterval(interval)
+  }, [isActive, seconds]);
+
+
+  return (
+    <div className="app">
+      <div className="time">
+        {seconds}
       </div>
-    );
-  };
-
-
-
+      <div className="row">
+        <button className="toggle" onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="stop" onClick={stop}>
+          Stop
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
