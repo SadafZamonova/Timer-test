30d9ddbdb129a7d876ec0c14fae9c690b697e314
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2022-02-28 12:45:21 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import moment from "moment";
 
-const TICK_TIME = 5;
+const TICK_TIME = moment().format('HH:mm:ss');;
 
 function App() {
   let savedTick = parseInt(localStorage.getItem("tick") || TICK_TIME)
   const [seconds, setSeconds] = useState(savedTick);
   const [isActive, setIsActive] = useState(false);
+  const [text, setText] = useState()
 
   function toggle() {
     setIsActive(!isActive);
+    setText('вы нажали на старт')
+
   }
 
   function stop() {
     setIsActive(false);
     setSeconds(TICK_TIME);
+    setText('вы нажали на стоп')
   }
 
   useEffect(() => {
@@ -51,7 +56,11 @@ function App() {
           Stop
         </button>
       </div>
+      <div>
+        <h2> {text} </h2>
+      </div>
     </div>
+  
   );
 };
 
